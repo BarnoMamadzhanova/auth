74125da9f7277aaa6870ea4ff34c1d0a1e8aba95
BarnoMamadzhanova <b@mamadzhanova.com>
2024-07-03 16:32:25 +0200

    added hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import classes from "./Hero.module.css";
+import hero from "../../assets/hero.svg";
+
+export const Hero = () => {
+  return (
+    <div className={classes.hero}>
+      <img src={hero} alt="Lorby" />
+      <div className={classes.info__box}>
+        <h2>Lorby</h2>
+        <p>Твой личный репетитор</p>
+      </div>
+    </div>
+  );
+};
