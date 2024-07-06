edb7cde04a9cb9003656039e2d086f9460ad7061
BarnoMamadzhanova <b@mamadzhanova.com>
2024-07-06 22:17:40 +0200

    profile page is done and styled

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import classes from "./Profile.module.css";
+import { hero } from "../../assets";
+import { Link } from "react-router-dom";
 
 function Profile() {
   return (
-    <div>
-      <p>Profile page!</p>
+    <div className={classes.profile}>
+      <div className={classes.profileInfo}>
+        <h2 className={classes.profileTitle}>С возвращением!</h2>
+        <p className={classes.profileText}>Lorby - твой личный репетитор</p>
+      </div>
+      <img src={hero} alt="hero" className={classes.profileImg} />
+      <Link className={classes.profileLink}>Выйти</Link>
     </div>
   );
 }
