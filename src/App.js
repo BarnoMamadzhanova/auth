bb9adeb6d0151adc278d8791959471bf4766b6bb
BarnoMamadzhanova <b@mamadzhanova.com>
2024-07-12 15:38:52 +0200

    uncommented protected routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import Registration from "./pages/Registration/Registration";
 import Confirmation from "./pages/Confirmation/Confirmation";
-// import withAuthProtection from "./components/HOC/withAuthProtection";
+import withAuthProtection from "./components/HOC/withAuthProtection";
 import { Navigate } from "react-router-dom";
 import "./styles/App.css";
 
-// const ProtectedProfile = withAuthProtection(Profile);
-// const ProtectedConfirmation = withAuthProtection(Confirmation);
+const ProtectedProfile = withAuthProtection(Profile);
+const ProtectedConfirmation = withAuthProtection(Confirmation);
 
 function App() {
   return (
@@ -17,10 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        {/* <Route path="/profile" element={<ProtectedProfile />} /> */}
-        {/* <Route path="/confirmation" element={<ProtectedConfirmation />} /> */}
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/confirmation" element={<Confirmation />} />
+        <Route path="/profile" element={<ProtectedProfile />} />
+        <Route path="/confirmation" element={<ProtectedConfirmation />} />
         <Route path="/confirm/:token" component={Confirmation} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
